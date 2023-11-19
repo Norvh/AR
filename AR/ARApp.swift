77e3f7f2b23c16239478e250af298b44ffd9ac20
Norvh <norah.wadani@gmail.com>
2023-11-19 10:32:51 +0300

    Initial Commit

---
+++ AR/ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARApp.swift
+//  AR
+//
+//  Created by Norah W on 05/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR/chos1.swift
@@ -0,0 +1,18 @@
+//
+//  chos1.swift
+//  AR
+//
+//  Created by Norah W on 08/05/1445 AH.
+//
+
+import SwiftUI
+
+struct chos1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    chos1()
+}
